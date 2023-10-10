401ba7b5d4073f5e4b5eb1d3db3b5fa2dc5d2ca5
aslskks <130728002+aslskks@users.noreply.github.com>
2023-10-09 21:11:45 -0600

    code

--- .py
+++ .py
@@ -1,8 +1,8 @@
 import random as rd
 from time import sleep
 while True:
-	nd1 = rd.randint(1,15)
-	nd2 = rd.randint(1,15)
+	nd1 = rd.randint(1,6)
+	nd2 = rd.randint(1,6)
 	r = nd1 + nd2
 	print(f"el numero es {r}")
 	sleep(1)
